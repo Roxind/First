544b93876471cf8ccb99a586ffe63fa51a750b91
Roxin <roxin_d@163.com>
2018-07-05 23:00:45 +0800

    creat a git

---
+++ app/src/main/java/com/example/roxin/firstapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.roxin.firstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    String msg = "Android : ";
+    /*当活动第一次被创建时调用*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
+    }
+
+    /** 当活动即将可见时调用 */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    /** 当活动可见时调用 */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /** 当其他活动获得焦点时调用 */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /** 当活动不再可见时调用 */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** 当活动将被销毁时调用 */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+}
+
