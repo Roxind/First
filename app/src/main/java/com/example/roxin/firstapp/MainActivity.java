2a838268ae706979bb93ba03ae39c120bcc51adf
Roxin <roxin_d@163.com>
2018-07-06 22:27:49 +0800

    add some edittext

--- app/src/main/java/com/example/roxin/firstapp/MainActivity.java
+++ app/src/main/java/com/example/roxin/firstapp/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
     String msg = "Android : ";
@@ -36,19 +39,19 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         Log.d(msg, "The onPause() event");
     }
-//
-//    /** 当活动不再可见时调用 */
-//    @Override
-//    protected void onStop() {
-//        super.onStop();
-//        Log.d(msg, "The onStop() event");
-//    }
-//
-//    /** 当活动将被销毁时调用 */
-//    @Override
-//    public void onDestroy() {
-//        super.onDestroy();
-//        Log.d(msg, "The onDestroy() event");
-//    }
+
+    /** 当活动不再可见时调用 */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** 当活动将被销毁时调用 */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
 }
 
